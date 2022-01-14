b9a06fe5078a29e4a1170a614f1182c390440503
mattavireddy <mattavireddycoder@outlook.com>
2022-01-14 13:51:42 +0530

    UI initial commit, added reactBootStrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 function App() {
+
+  const [activites, setActivites] = useState([]);
+
+  useEffect(() => {
+    console.log(activites)
+
+    axios.get("http://localhost:5000/api/Activities").then(response => {
+      console.log(response.data)
+      setActivites(response.data);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ListGroup>
+        {
+          activites.map((a: any) => 
+            <ListGroupItem key={a.id}>{a.title} </ListGroupItem>  
+          )
+        }
+      </ListGroup>
     </div>
   );
 }
